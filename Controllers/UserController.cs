3d79e41d8f87eec2c2958d8630418de6eae4cc29
JulioRobot <juliorobot16@gmail.com>
2025-05-23 15:27:17 +0700

    try to install again bootstrap

---
+++ Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using contentCreator.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace contentCreator.Controllers
+{
+    public class UserController: Controller
+    {
+        public IActionResult Create()
+        { 
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(UserModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Success");
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ Models/UserModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace contentCreator.Models
+{
+    public class UserModel
+    {
+        [Required(ErrorMessage = "Name is required")]
+        [Display(Name = "Full Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
